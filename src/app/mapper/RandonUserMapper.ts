10ab7be453e8920da85e7be4562438f8da19de64
Victor Tapia <victortapiaegana@gmail.com>
2025-05-26 21:48:40 -0400

    feat: agregar método ResponseMapper y corregir ResponseUserConvert en RandomUserMapper

--- src/app/mapper/RandonUserMapper.ts
+++ src/app/mapper/RandonUserMapper.ts
@@ -1,8 +1,28 @@
-import { RandonUserResponse } from './../interface/RandonUserInterface';
+import { RandonUserResponse, RandomUserType, Data } from './../interface/RandonUserInterface';
 export class RandomUserMapper{
 
-  ResponseUserConvert<RandonUserResponse[]>(){
-    return
+  ResponseMapper(item:Data):RandomUserType{
+
+    return{
+        id:item.login.uuid,
+        nombre:`${item.name.first}  ${item.name.last}`,
+        genero:item.gender,
+        email:item.email,
+        telefono:item.cell,
+        imagen:item.picture.large,
+        direccion:item.location.street.name,
+        ciudad:item.location.city,
+        pais:item.location.country,
+        latitud:item.location.coordinates.latitude,
+        longitud:item.location.coordinates.longitude
+    }
+
+  }
+
+  ResponseUserConvert(userlist:Data[]):RandomUserType[]{
+    return(
+        userlist.map((item)=>this.ResponseMapper(item))
+    )
   }
 
 }
